d8a3cdb97901b7eec77e5e31a91400c2dd5f477d
AskaryanKarine <askariyan.karine@gmail.com>
2022-04-01 15:34:40 +0300

    add lab 4 (cnahge 3 lab's interface)

---
+++ lab_04/mainwindow.h
@@ -0,0 +1,98 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QGraphicsScene>
+#include <QPushButton>
+#include <QLabel>
+#include <stack>
+
+typedef QGraphicsScene *canvas_t;
+
+enum method_t
+{
+    STANDART,
+    CANONICAL,
+    PARAMETRIC,
+    BRESEN,
+    MIDDLE_POINT,
+};
+
+enum figure_type_t
+{
+    CIRCLE,
+    ELLIPSE,
+};
+
+struct figure_t
+{
+    QPointF start;
+    double r1;
+    double r2;
+    figure_type_t type;
+    QColor color;
+    method_t method;
+};
+
+struct spectre_t
+{
+    QPointF center;
+    double angle;
+    double radius;
+    QColor color;
+    method_t method;
+};
+
+struct content_t
+{
+    std::vector<figure_t> lines;
+    std::vector<spectre_t> spectres;
+    QColor back_color;
+};
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+    bool eventFilter(QObject *object, QEvent *event);
+private slots:
+    void on_pushButton_back_color_clicked();
+    void app_info_show();
+    void author_info_show();
+    void exit_show();
+    void show_color(QColor color, QLabel *button);
+    void on_pushButton_line_color_clicked();
+    void on_pushButton_line_clicked();
+    void on_pushButton_spectrum_clicked();
+    void on_pushButton_clear_clicked();
+    void on_pushButton_cancel_clicked();
+    void drawing_axes();
+    void showEvent(QShowEvent *ev);
+    void windowShown();
+    void drawing_content();
+    void wheelEvent(QWheelEvent *event);
+    void resizeEvent(QResizeEvent *event);
+    void on_pushButton_reset_scale_clicked();
+//    int drawing_line(line_t &line, bool is_drawing, bool is_cnt_steps);
+    void drawing_spectrum(spectre_t &spectrum);
+    void on_pushButton_time_clicked();
+    double measure_avg_time(spectre_t spectrum);
+
+private:
+    Ui::MainWindow *ui;
+    QGraphicsScene *scene;
+    content_t data;
+    QColor line_color = Qt::black;
+    QColor back_color = Qt::white;
+    std::stack<content_t> cancel;
+};
+#endif // MAINWINDOW_H
