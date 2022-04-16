08460cb9291ca82ad6423830b5a2c3b6b2e1cfe0
AskaryanKarine <askariyan.karine@gmail.com>
2022-04-17 00:19:50 +0300

    add some interface func

--- lab_05/mainwindow.cpp
+++ lab_05/mainwindow.cpp
@@ -1,15 +1,130 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
+#include <QColorDialog>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QAction *AboutProgAction = ui->menubar->addAction("О программе");
+    connect(AboutProgAction, SIGNAL(triggered()), this, SLOT(app_info_show()));
+    QAction *AboutAuthorAction = ui->menubar->addAction("Об авторе");
+    connect(AboutAuthorAction, SIGNAL(triggered()), this, SLOT(author_info_show()));
+    QAction *ExitAction = ui->menubar->addAction(("Выход"));
+    connect(ExitAction, SIGNAL(triggered()), this, SLOT(exit_show()));
+
+    // все, что связано со сценой
+    scene = new QGraphicsScene(this);
+    ui->graphicsView->setScene(scene);
+    ui->graphicsView->setAlignment(Qt::AlignTop | Qt::AlignLeft);
+    ui->graphicsView->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    ui->graphicsView->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+
+    // отключить кнопки и задержку
+    ui->pushButton_cancel->setEnabled(false);
+    ui->spinBox->setEnabled(false);
+
+    // установка цветов
+    show_color(back_color, ui->label_bc);
+    show_color(line_color, ui->label_lc);
+    show_color(fill_color, ui->label_fc);
+
+    // начальные условия таблицы
+    ui->tableWidget->setColumnCount(2);
+    ui->tableWidget->setHorizontalHeaderLabels(QStringList() << "x" << "y");
+    ui->tableWidget->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+    delete scene;
+}
+
+void MainWindow::app_info_show()
+{
+    QMessageBox::information(NULL, "О программе","Реализация и исследование алгоритмов растрового заполнения сплошных областей");
+}
+
+void MainWindow::author_info_show()
+{
+    QMessageBox::information(NULL, "Об авторе", " Программа написана студенткой \nАскарян Каринэ из группы ИУ7-42Б\n +7(916)888-02-20");
+}
+
+void MainWindow::exit_show()
+{
+    QMessageBox msg_quit;
+    msg_quit.setText("Работа с программой будет завершена");
+    msg_quit.setStandardButtons(QMessageBox::Ok | QMessageBox::Cancel);
+    msg_quit.setIcon(QMessageBox::Warning);
+    msg_quit.setDefaultButton(QMessageBox::Ok);
+    msg_quit.setWindowTitle("Предупреждение!");
+    int rc = msg_quit.exec();
+
+    if (rc == QMessageBox::Ok)
+        qApp->quit();
+}
+
+void MainWindow::error_message(QString str)
+{
+    QMessageBox::critical(NULL, "Ошибка", str);
+}
+
+void MainWindow::show_color(QColor color, QLabel *lab)
+{
+    QImage im = QImage(lab->geometry().width(), lab->geometry().height(), QImage::Format_RGB32);
+    QPainter p(&im);
+    p.setBrush(QBrush(color));
+    p.setPen(Qt::black);
+    QRect rect = QRect(0, 0, lab->geometry().width(), lab->geometry().height());
+    p.drawRect(rect);
+
+    QPixmap pixmap = QPixmap::fromImage(im);
+    lab->clear();
+    lab->setPixmap(pixmap);
+}
+
+void MainWindow::color_dialog(QColor &color)
+{
+    QColorDialog dialog;
+    dialog.setCurrentColor(line_color);
+    dialog.show();
+    dialog.exec();
+    QColor tmp = dialog.selectedColor();
+    if (!tmp.isValid())
+        error_message("Выберите цвет");
+    else
+        color = tmp;
+}
+
+void MainWindow::on_pushButton_back_color_clicked()
+{
+    color_dialog(back_color);
+    show_color(back_color, ui->label_bc);
+}
+
+
+void MainWindow::on_pushButton_line_color_clicked()
+{
+    color_dialog(line_color);
+    show_color(line_color, ui->label_lc);
+}
+
+
+void MainWindow::on_pushButton_fill_color_clicked()
+{
+    color_dialog(fill_color);
+    show_color(fill_color, ui->label_fc);
+}
+
+
+void MainWindow::on_comboBox_activated(int index)
+{
+    if (index == 1)
+        ui->spinBox->setEnabled(true);
+    else
+        ui->spinBox->setEnabled(false);
 }
 
--- lab_05/mainwindow.h
+++ lab_05/mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QColor>
+#include <QLabel>
+#include "structurs.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +18,26 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void app_info_show();
+    void author_info_show();
+    void exit_show();
+    void error_message(QString str);
+    void show_color(QColor color, QLabel *lab);
+    void color_dialog(QColor &color);
+    void on_pushButton_back_color_clicked();
+
+    void on_pushButton_line_color_clicked();
+
+    void on_pushButton_fill_color_clicked();
+
+    void on_comboBox_activated(int index);
+
 private:
     Ui::MainWindow *ui;
+    canvas_t scene;
+    QColor back_color = Qt::white;
+    QColor line_color = Qt::black;
+    QColor fill_color = Qt::black;
 };
 #endif // MAINWINDOW_H
