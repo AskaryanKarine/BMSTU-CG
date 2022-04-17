f560674ace611c2ec96c0b1ec902b3025e9753fd
AskaryanKarine <askariyan.karine@gmail.com>
2022-04-18 01:26:47 +0300

    add description structurs

--- lab_05/structurs.h
+++ lab_05/structurs.h
@@ -2,8 +2,38 @@
 #define STRUCTURS_H
 
 #include <QGraphicsScene>
+#include <QGraphicsView>
 
 typedef QGraphicsScene *canvas_t;
 typedef QGraphicsView *gv_t;
+
+struct point
+{
+    int x;
+    int y;
+};
+
+struct hole
+{
+    std::vector<point> points;
+    bool is_closed_hole;
+};
+
+struct figure
+{
+    std::vector<point> main_figure;
+    std::vector<hole> holes;
+    QColor line_color;
+    QColor fill_color;
+    bool is_closed_figure;
+};
+
+struct content
+{
+    std::vector<figure> figures;
+    QColor back_color;
+    int n_figures;
+    int n_holes;
+};
 
 #endif // STRUCTURS_H
