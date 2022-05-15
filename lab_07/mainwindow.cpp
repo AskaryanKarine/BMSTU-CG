deaeff469f575a02cb3222fb4904a01eac33ab3f
AskaryanKarine <askariyan.karine@gmail.com>
2022-05-15 23:06:01 +0300

    added work with colors

--- lab_07/mainwindow.cpp
+++ lab_07/mainwindow.cpp
@@ -1,11 +1,29 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QColorDialog>
+#include <QMessageBox>
 
-MainWindow::MainWindow(QWidget *parent)
+MainWindow::MainWindow(QWidget* parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    // пункты меню
+    QAction* AboutProgAction = ui->menubar->addAction("О программе");
+    connect(AboutProgAction, SIGNAL(triggered()), this, SLOT(app_info_show()));
+    QAction* AboutAuthorAction = ui->menubar->addAction("Об авторе");
+    connect(AboutAuthorAction, SIGNAL(triggered()), this, SLOT(author_info_show()));
+    QAction* ExitAction = ui->menubar->addAction(("Выход"));
+    connect(ExitAction, SIGNAL(triggered()), this, SLOT(exit_show()));
+
+    // начальные цвета
+    data.cut_color = QColor("#55ffff");
+    data.line_color = Qt::black;
+    data.visible_color = QColor("#ff5500");
+
+    show_color(data.cut_color, ui->label_cc);
+    show_color(data.line_color, ui->label_lc);
+    show_color(data.visible_color, ui->label_vlc);
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +31,80 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+// информационные функции
+void MainWindow::app_info_show()
+{
+    QMessageBox::information(NULL, "О программе", "Реализация и исследование простого алгоритма отсечения отрезка регулярным отсекателем.\n\n\
+Интерфейс этой программы был разработан с использванием ресурсов с сайта Flaticon.com\n\n\
+This cover has been designed using resources from Flaticon.com");
+}
+
+void MainWindow::author_info_show()
+{
+    QMessageBox::information(NULL, "Об авторе", " Программа написана студенткой \nАскарян Каринэ из группы ИУ7-42Б\n +7(916)888-02-20");
+}
+
+void MainWindow::exit_show()
+{
+    QMessageBox msg_quit;
+    msg_quit.setText("Работа с программой будет завершена");
+    msg_quit.setStandardButtons(QMessageBox::Ok | QMessageBox::Cancel);
+    msg_quit.setIcon(QMessageBox::Warning);
+    msg_quit.setDefaultButton(QMessageBox::Ok);
+    msg_quit.setWindowTitle("Предупреждение!");
+    int rc = msg_quit.exec();
+
+    if (rc == QMessageBox::Ok)
+        qApp->quit();
+}
+
+void MainWindow::error_message(QString str)
+{
+    QMessageBox::critical(NULL, "Ошибка", str);
+}
+
+// работа с цветом
+void MainWindow::show_color(QColor color, QLabel* lab)
+{
+    QImage im = QImage(lab->geometry().width(), lab->geometry().height(), QImage::Format_RGB32);
+    QPainter p(&im);
+    p.setBrush(QBrush(color));
+    p.setPen(Qt::black);
+    QRect rect = QRect(0, 0, lab->geometry().width(), lab->geometry().height());
+    p.drawRect(rect);
+
+    QPixmap pixmap = QPixmap::fromImage(im);
+    lab->clear();
+    lab->setPixmap(pixmap);
+}
+
+void MainWindow::color_dialog(QColor& color)
+{
+    QColorDialog dialog;
+    dialog.setCurrentColor(color);
+    dialog.show();
+    dialog.exec();
+    QColor tmp = dialog.selectedColor();
+    if (!tmp.isValid())
+        error_message("Выберите цвет");
+    else
+        color = tmp;
+}
+
+void MainWindow::on_pushButton_line_color_clicked()
+{
+    color_dialog(data.line_color);
+    show_color(data.line_color, ui->label_lc);
+}
+
+void MainWindow::on_pushButton_cut_color_clicked()
+{
+    color_dialog(data.cut_color);
+    show_color(data.cut_color, ui->label_cc);
+}
+
+void MainWindow::on_pushButton_visible_line_color_clicked()
+{
+    color_dialog(data.visible_color);
+    show_color(data.visible_color, ui->label_vlc);
+}
--- lab_07/mainwindow.h
+++ lab_07/mainwindow.h
@@ -1,21 +1,45 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include "structurs.h"
+#include <QColor>
+#include <QLabel>
 #include <QMainWindow>
+#include <stack>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui {
+class MainWindow;
+}
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    MainWindow(QWidget* parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void app_info_show();
+    void author_info_show();
+    void exit_show();
+    void error_message(QString str);
+    void show_color(QColor color, QLabel* lab);
+
+    void color_dialog(QColor& color);
+    void on_pushButton_line_color_clicked();
+
+    void on_pushButton_cut_color_clicked();
+
+    void on_pushButton_visible_line_color_clicked();
+
 private:
-    Ui::MainWindow *ui;
+    Ui::MainWindow* ui;
+    content data;
+    std::stack<content> cancel;
+    //    QColor line_color = Qt::black;
+    //    QColor visible_color = Qt::red;
+    //    QColor cut_color = Qt::blue;
 };
 #endif // MAINWINDOW_H
