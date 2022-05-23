6f4587a8b64f4c125ac666dbc52fd2a8373183d3
AskaryanKarine <askariyan.karine@gmail.com>
2022-05-24 00:45:39 +0300

    added some func in structurs

--- lab_08/structurs.h
+++ lab_08/structurs.h
@@ -8,17 +8,19 @@ typedef QGraphicsScene* canvas_t;
 typedef QGraphicsView* gv_t;
 
 struct point {
-    int x = -1;
-    int y = -1;
+    int x = 0;
+    int y = 0;
 
     bool is_null() { return !(x > 0 && y > 0); };
     bool is_null() const { return !(x > 0 && y > 0); };
     bool operator==(const point& p) { return x == p.x && y == p.y; };
     bool operator!=(const point& p) { return !operator==(p); };
     point operator-(const point& p) const { return { x - p.x, y - p.y }; };
+    point operator+(const point &p) const {return {x + p.x, y + p.y}; };
+    point operator*(const int &num) { return {x * num, y * num}; };
 };
 
-struct figure {
+struct line {
     point p1;
     point p2;
 
@@ -26,24 +28,34 @@ struct figure {
     bool is_full() const { return !(p1.is_null() || p2.is_null()); };
 };
 
-struct cut_rect {
+struct polygon {
     std::vector<point> points;
+    std::vector<line> lines = {};
     bool is_close;
 
-    bool check_cut();
+    void update_lines(const point &p)
+    {
+        if (lines.empty()) {
+            lines.push_back({});
+            lines[0].p1 = p;
+        } else {
+            size_t last = lines.size() - 1;
+            lines[last].p2 = p;
+            if (!is_close) {
+                lines.push_back({});
+                lines[++last].p1 = p;
+            }
+        }
+    }
 };
 
 struct content {
-    cut_rect cut;
-    std::vector<figure> lines;
-    std::vector<figure> visible_lines;
+    polygon cut;
+    std::vector<line> lines;
+    std::vector<line> visible_lines;
     QColor line_color;
     QColor visible_color;
     QColor cut_color;
-    int number_cut = 0;
 };
-
-
-
 
 #endif // STRUCTURS_H
